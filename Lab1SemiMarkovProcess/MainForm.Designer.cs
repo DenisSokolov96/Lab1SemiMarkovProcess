4a59917af6a725c856e5e61d127bb238cf7c789b
DenisSokolov96 <dens111296@gmail.com>
2020-10-04 16:07:31 +0300

    первая лабораторка

--- Lab1SemiMarkovProcess/MainForm.Designer.cs
+++ Lab1SemiMarkovProcess/MainForm.Designer.cs
@@ -31,6 +31,24 @@
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.запуститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -39,9 +57,10 @@
             this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox1.Location = new System.Drawing.Point(12, 116);
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 179);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(599, 334);
+            this.richTextBox1.Size = new System.Drawing.Size(848, 271);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
@@ -51,7 +70,7 @@
             this.запуститьToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(624, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(873, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -60,12 +79,184 @@
             this.запуститьToolStripMenuItem.Name = "запуститьToolStripMenuItem";
             this.запуститьToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
             this.запуститьToolStripMenuItem.Text = "Запустить";
+            this.запуститьToolStripMenuItem.Click += new System.EventHandler(this.запуститьToolStripMenuItem_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 50);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(125, 73);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "0,5 0,25 0,25\n0,5 0 0,5\n0,67 0,33 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Матрица вероятностей";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Вывод:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(200, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Время";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(316, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Параметр - время";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(459, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Стартовый вектор";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(606, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Матрица времени";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(749, 28);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Время параметр";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(159, 50);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(125, 73);
+            this.richTextBox3.TabIndex = 10;
+            this.richTextBox3.Text = "2\n1\n1";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(303, 50);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(125, 73);
+            this.richTextBox4.TabIndex = 11;
+            this.richTextBox4.Text = "2\n5\n8";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(447, 50);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(125, 73);
+            this.richTextBox5.TabIndex = 12;
+            this.richTextBox5.Text = "0,3333\n0,3333\n0,3333";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Location = new System.Drawing.Point(590, 50);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(125, 73);
+            this.richTextBox6.TabIndex = 13;
+            this.richTextBox6.Text = "2 2 2\n2 2 2\n2 2 2";
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.Location = new System.Drawing.Point(735, 50);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.Size = new System.Drawing.Size(125, 73);
+            this.richTextBox7.TabIndex = 14;
+            this.richTextBox7.Text = "1 5 9\n3 6 8\n7 2 1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 139);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Кол-во итераций:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 136);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 20);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.Text = "1000";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(304, 150);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(556, 23);
+            this.progressBar1.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(300, 134);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(119, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Процесс выполнения:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(426, 134);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 19;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(624, 462);
+            this.ClientSize = new System.Drawing.Size(873, 462);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -83,6 +274,24 @@
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem запуститьToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
 
--- Lab1SemiMarkovProcess/MainForm.cs
+++ Lab1SemiMarkovProcess/MainForm.cs
@@ -7,14 +7,180 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Lab1SemiMarkovProcess
 {
     public partial class MainForm : Form
     {
+        //вероятности переходов
+        private List<double[]> listStahM = new List<double[]>();
+        //стартовые типы переходов
+        private int[] masTime;
+        //стартовые типы переходов
+        private int[] masTimeParam;
+        //стартовые вероятности переходов
+        private double[] startMas;
+        //типы переходов
+        private List<int[]> listMTime = new List<int[]>();
+        //параметры переходов
+        private List<int[]> listTParam = new List<int[]>();
+        //матрица статистики переходов
+        private List<int[]> matrStatist = new List<int[]>();
+        //кол-во итераций
+        private int countIter = 0;
+        private bool flag;
+
         public MainForm()
         {
             InitializeComponent();
         }
+
+        private void запуститьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            readDatas();
+            progressBar1.Value = 10;
+            label10.Text = progressBar1.Value.ToString() + "%";
+            if (!flag) computation();
+            progressBar1.Value = 100;
+            label10.Text = progressBar1.Value.ToString() + "%";
+            richTextBox1.SelectionStart = richTextBox1.TextLength;
+            richTextBox1.ScrollToCaret();
+        }
+
+        //основная функция
+        private void computation()
+        {
+            double[] variants = startMas.ToArray();
+            int[] times = masTime.ToArray();
+            int k = 0;
+            int t = -1;
+            int proc = countIter / 90;
+            while (k < countIter)
+            {
+                int updateT = rndSelect(variants);
+                double jumpT = timeJumpFunc(t, updateT, times[updateT]);
+                funWrite(k+1, variants, updateT+1, jumpT, t+1);
+
+                variants = listStahM[updateT];
+                times = listMTime[updateT];
+
+                t = updateT;
+
+                k++;
+                proc--;
+                if (proc == 0)
+                {
+                    proc = countIter / 90;
+                    progressBar1.Value++;
+                    label10.Text = progressBar1.Value.ToString() + "%";
+                }
+            }
+
+            for (int i = 0; i < matrStatist.Count; i++)
+            {
+                for (int j = 0; j < matrStatist[i].Length; j++)
+                    richTextBox1.Text += matrStatist[i][j].ToString() + " ";
+                richTextBox1.Text += "\n";
+            }
+
+        }
+
+        private int rndSelect(double[] mas)
+        {
+            double a = mas.Sum();
+            if (a < 1) mas[0] += 1 - mas[0];
+
+            Random rnd = new Random();
+            int value = rnd.Next(0, mas.Length);
+
+            return value;
+        }
+
+        private double timeJumpFunc(int t, int updateT, int zn)
+        {
+            if (t == -1)
+            {
+                switch (zn) {
+                    case 1: return masTimeParam[updateT];
+                    case 2: return rnd_func(masTimeParam[updateT]);
+                    default: break;
+                }
+            }
+            switch (zn)
+            {
+                case 1: return listTParam[t][updateT];
+                case 2: return rnd_func(listTParam[t][updateT]);
+                default: break;
+            }
+
+            return 0;
+        }
+
+        private double rnd_func(int data)
+        {
+            /*
+             if type(data) == list:
+            data = data[0]
+        return random.expovariate(data)*/
+            return (data < 0 ? 0 : 1 - Math.Exp(-1 / data));
+        }
+
+        private void funWrite(int k, double[] variants, int updateT, double jumpT, int t)
+        {
+            if (t < 1 || t > 2) t = t % matrStatist.Count;
+            if (updateT < 1 || updateT > 2) updateT = updateT % matrStatist[t].Length;
+
+            matrStatist[t][updateT] += 1;
+            richTextBox1.Text += k.ToString() + ") \t" + t.ToString() + " --> " + updateT.ToString() + 
+                                " ( " + Math.Round(jumpT, 3).ToString() + " ) " + "\t[ ";
+            for (int i = 0; i < variants.Length-1; i++)
+                richTextBox1.Text += variants[i] + " ";
+            richTextBox1.Text += variants[variants.Length-1] + " ]";
+            richTextBox1.Text += "\n";
+        }
+
+        //получение данных
+        private void readDatas()
+        {           
+            try
+            {
+                countIter = Convert.ToInt32(textBox1.Text);
+
+                for (int i = 0; i < richTextBox2.Lines.Length; i++)                              
+                    listStahM.Add(Array.ConvertAll(richTextBox2.Lines[i].Split(' '), Double.Parse));
+
+                masTime = new int[richTextBox3.Lines.Length];
+                for (int i = 0; i < richTextBox3.Lines.Length; i++)
+                    masTime[i] = Convert.ToInt32(richTextBox3.Lines[i]);
+
+                masTimeParam = new int[richTextBox4.Lines.Length];
+                for (int i = 0; i < richTextBox4.Lines.Length; i++)
+                    masTimeParam[i] = Convert.ToInt32(richTextBox4.Lines[i]);
+
+                startMas = new double[richTextBox5.Lines.Length];
+                for (int i = 0; i < richTextBox5.Lines.Length; i++)
+                    startMas[i] = Convert.ToDouble(richTextBox5.Lines[i]);
+               
+                for (int i = 0; i < richTextBox6.Lines.Length; i++)
+                    listMTime.Add(richTextBox6.Lines[i].Split(' ').Select(int.Parse).ToArray());
+
+                for (int i = 0; i < richTextBox7.Lines.Length; i++)
+                    listTParam.Add(richTextBox7.Lines[i].Split(' ').Select(int.Parse).ToArray());
+
+                for (int i = 0; i < listStahM.Count(); i++)
+                {
+                    int[] mas = new int[listStahM[i].Length];
+                    for (int j = 0; j < listStahM[i].Length; j++)                    
+                        mas[j] = 0;                    
+                    matrStatist.Add(mas);
+                }
+            }
+            catch(Exception e) {
+                MessageBox.Show(e.Message, "Info", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                flag = true;
+            }
+        }
+
     }
 }
